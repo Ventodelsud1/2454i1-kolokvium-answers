272200fd0232c7ed2e87f2f446dd7fd4fd8bce4a
Ventodelsud1 <nidjat.axundov@gmail.com>
2025-03-18 00:46:19 +0400

    first commit

---
+++ 1 question.py
@@ -0,0 +1,9 @@
+inventory = {}
+for _ in range(3):
+#Adding all information
+    product = input("Add your product: ")
+    quantity = int(input("Add hpw much of product: "))
+    inventory[product] = quantity
+
+#And how to write it after?
+#To use if and break?

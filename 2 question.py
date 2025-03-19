29343cde88669cea6faac8b3ec6328b09b95f66d
Ventodelsud1 <nidjat.axundov@gmail.com>
2025-03-19 11:44:34 +0400

    first commit

--- 2 question.py
+++
@@ -1,8 +0,0 @@
-def is_sorted(nums):
-    for i in range(2, len(nums)):
-        if nums[i] < nums[i - 1]:
-            return False
-    return True
-
-numbers = [2, 5, 2, 3, 4]
-print(is_sorted(numbers))
